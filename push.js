152bbdf78f2d13ff4d2a49a78208e9c835bb6f0d
dieguitojd <diegoaw591@gmail.com>
2022-03-31 17:03:09 -0600

    modify serviceworker, add endpoint

--- push.js
+++ push.js
@@ -1,13 +1,25 @@
-var push = require('web-push');
+var push = require("web-push");
 
-let vapIdKeys =  {
-    publicKey: 'BBKAMx_AXG49_kRr4CWcD6PND2kPhq1xs7sRYizLTIr3SWvX773rmzhVNrfeKyf-mUvrRlOthentfm1Huo9FNYU',
-    privateKey: 'nOsczMCLlz1PNPsKAxHVVReZ2hRAfvi1-p8B7ALlLhs'
-  }
+let vapIdKeys = {
+  publicKey:
+    "BBKAMx_AXG49_kRr4CWcD6PND2kPhq1xs7sRYizLTIr3SWvX773rmzhVNrfeKyf-mUvrRlOthentfm1Huo9FNYU",
+  privateKey: "nOsczMCLlz1PNPsKAxHVVReZ2hRAfvi1-p8B7ALlLhs",
+};
 
-push.setVapidDetails('mailto:diegoaw591@gmail.com', vapIdKeys.publicKey, vapIdKeys.privateKey);
+push.setVapidDetails(
+  "mailto:diegoaw591@gmail.com",
+  vapIdKeys.publicKey,
+  vapIdKeys.privateKey
+);
 
 let sub = {
-
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/e7vItTbXjNI:APA91bE7i6B0kzXOgExVgXcdiAhvY2hERg1tZLc7N4f-B50dws3NhlBO5EHQ3LGQ9aMsvXxqi2YNiRE6DpFq_O68GCsTO-kpW3VOaLC91j75szaQEgksBRZdQGpV4Aw_ABSYWiM7pQhy",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BAkSchAQJ3PVuslTZd1ztkqYuUEdxRj-EkTbDdCU2nrNansytIGpiOnkd5Cnbm-7Siwqxkp3j3flD9UZpT52Nts",
+    auth: "QrdxNFeJpJPhrwLNeVOoEw",
+  },
 };
-push.sendNotification(sub, 'test message');
+push.sendNotification(sub, "test message");
--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('test', {});
+self.addEventListener("push", function (event) {
+  const options = {
+    body: "Yay it works.",
+    icon: "images/icon.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "explore service",
+        icon: "images/checkmark.png",
+      },
+      { action: "close", title: "Close", icon: "images/xmark.png" },
+    ],
+  };
+
+  event.waitUntil(self.registration.showNotification('Hello world', options));
 });
